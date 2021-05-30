bfb7fa56e04fb727f18e1e94fe0208cc642a2033
LiKaibin <2766686505@qq.com>
2021-05-30 09:43:46 +0800

    修改Test文件

--- day01/src/com/itheima/demo1_面向对象/Test.java
+++ day01/src/com/itheima/demo1_面向对象/Test.java
@@ -1,11 +1,4 @@
-package com.itheima.demo1_面向对象;
 
-
-import java.util.Arrays;
-
-//需求：打印数组中所有的元素，打印格式为：[元素1，元素2，元素3，...元素n]
-public class Test {
-    public static void main(String[] args) {
 
         //面向过程
         int [] arr = {10,20,30,40,50};
@@ -17,7 +10,7 @@ public class Test {
             }else {
                 System.out.print(arr[i]+",");
             }
-
+    
         }
 
 
